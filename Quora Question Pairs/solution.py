62d95ff9b9e1181b8f810dde02ab3937236b990c
Xingyang Ni <nixingyang@users.noreply.github.com>
2017-05-06 16:47:45 +0300

    Append the word frequency as a feature.

--- Quora Question Pairs/solution.py
+++ Quora Question Pairs/solution.py
@@ -158,10 +158,11 @@ def load_dataset():
         train_label_array = np.array(train_label_list, dtype=np.bool)
 
         print("Initiating embedding matrix ...")
-        embedding_matrix = np.zeros((len(tokenizer.word_index) + 1, word2vec.vector_size), dtype=np.float32)
+        embedding_matrix = np.zeros((len(tokenizer.word_index) + 1, word2vec.vector_size + 1), dtype=np.float32)
         for word, index in tokenizer.word_index.items():
             assert word in available_vocabulary
-            embedding_matrix[index] = word2vec.word_vec(word)
+            embedding_matrix[index] = word2vec.word_vec(word).tolist() + [tokenizer.word_counts[word]]
+        embedding_matrix[:, -1] /= np.max(embedding_matrix[:, -1])
         assert np.sum(np.isclose(np.sum(embedding_matrix, axis=1), 0)) == 1
 
         print("Saving dataset to disk ...")
