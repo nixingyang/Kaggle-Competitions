4c8285d4f4705450489ace4000a7167e831ba8d1
Xingyang Ni <nixingyang@users.noreply.github.com>
2017-05-10 21:00:21 +0300

    Remove magic feature.

--- Quora Question Pairs/solution.py
+++ Quora Question Pairs/solution.py
@@ -128,7 +128,7 @@ def clean_sentence(original_sentence, word_to_index_dict, known_typo_dict, resul
         print("Exception for {}: {}".format(original_sentence, exception))
         return result_when_failure
 
-def load_file(original_file_path, word_to_index_dict, known_typo_dict, unique_text_value_to_frequency_dict):
+def load_file(original_file_path, word_to_index_dict, known_typo_dict):
     processed_file_path = os.path.join(os.path.dirname(original_file_path), "processed_" + os.path.basename(original_file_path))
     if os.path.isfile(processed_file_path):
         print("Loading {} ...".format(processed_file_path))
@@ -137,22 +137,12 @@ def load_file(original_file_path, word_to_index_dict, known_typo_dict, unique_te
         print("Loading {} ...".format(original_file_path))
         file_content = pd.read_csv(original_file_path, encoding="utf-8")
 
-        if len(unique_text_value_to_frequency_dict) == 0:
-            print("Calculating text value to frequency dictionary ...")
-            unique_text_value_array, unique_text_count_array = np.unique(file_content["question1"].tolist() + file_content["question2"].tolist(), return_counts=True)
-            unique_text_frequency_array = unique_text_count_array.astype(np.float32) / np.max(unique_text_count_array)
-            unique_text_value_to_frequency_dict.update(dict(zip(unique_text_value_array, unique_text_frequency_array)))
-
-        print("Generating frequency columns ...")
-        file_content["question1_frequency"] = file_content["question1"].apply(lambda text_value: unique_text_value_to_frequency_dict.get(text_value, 0))
-        file_content["question2_frequency"] = file_content["question2"].apply(lambda text_value: unique_text_value_to_frequency_dict.get(text_value, 0))
-
         print("Cleaning sentences ...")
         file_content["processed_question1"] = file_content["question1"].apply(lambda original_sentence: clean_sentence(original_sentence, word_to_index_dict, known_typo_dict))
         file_content["processed_question2"] = file_content["question2"].apply(lambda original_sentence: clean_sentence(original_sentence, word_to_index_dict, known_typo_dict))
 
         print("Saving processed file ...")
-        interesting_column_name_list = ["processed_question1", "processed_question2", "question1_frequency", "question2_frequency"]
+        interesting_column_name_list = ["processed_question1", "processed_question2"]
         if "is_duplicate" in file_content.columns:
             interesting_column_name_list.append("is_duplicate")
         file_content = file_content[interesting_column_name_list]
@@ -160,32 +150,26 @@ def load_file(original_file_path, word_to_index_dict, known_typo_dict, unique_te
 
     question1_text_list = file_content["processed_question1"].tolist()
     question2_text_list = file_content["processed_question2"].tolist()
-    question1_frequency_list = file_content["question1_frequency"].tolist()
-    question2_frequency_list = file_content["question2_frequency"].tolist()
 
     if "is_duplicate" in file_content.columns:
         is_duplicate_list = file_content["is_duplicate"].tolist()
-        return question1_text_list, question2_text_list, question1_frequency_list, question2_frequency_list, is_duplicate_list
+        return question1_text_list, question2_text_list, is_duplicate_list
     else:
-        return question1_text_list, question2_text_list, question1_frequency_list, question2_frequency_list
+        return question1_text_list, question2_text_list
 
 def load_dataset():
     if os.path.isfile(DATASET_FILE_PATH):
         print("Loading dataset from disk ...")
         dataset_file_content = np.load(DATASET_FILE_PATH)
         train_data_1_array = dataset_file_content["train_data_1_array"]
-        train_frequency_1_array = dataset_file_content["train_frequency_1_array"]
         train_data_2_array = dataset_file_content["train_data_2_array"]
-        train_frequency_2_array = dataset_file_content["train_frequency_2_array"]
         test_data_1_array = dataset_file_content["test_data_1_array"]
-        test_frequency_1_array = dataset_file_content["test_frequency_1_array"]
         test_data_2_array = dataset_file_content["test_data_2_array"]
-        test_frequency_2_array = dataset_file_content["test_frequency_2_array"]
         train_label_array = dataset_file_content["train_label_array"]
         embedding_matrix = dataset_file_content["embedding_matrix"]
 
-        return train_data_1_array, train_frequency_1_array, train_data_2_array, train_frequency_2_array, \
-            test_data_1_array, test_frequency_1_array, test_data_2_array, test_frequency_2_array, train_label_array, embedding_matrix
+        return train_data_1_array, train_data_2_array, test_data_1_array, test_data_2_array, \
+            train_label_array, embedding_matrix
     else:
         print("Initiating word2vec ...")
         word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE_PATH, binary=True)
@@ -194,9 +178,8 @@ def load_dataset():
 
         print("Loading text files ...")
         known_typo_dict = {}
-        unique_text_value_to_frequency_dict = {}
-        train_text_1_list, train_text_2_list, train_frequency_1_list, train_frequency_2_list, train_label_list = load_file(TRAIN_FILE_PATH, word_to_index_dict, known_typo_dict, unique_text_value_to_frequency_dict)
-        test_text_1_list, test_text_2_list, test_frequency_1_list, test_frequency_2_list = load_file(TEST_FILE_PATH, word_to_index_dict, known_typo_dict, unique_text_value_to_frequency_dict)
+        train_text_1_list, train_text_2_list, train_label_list = load_file(TRAIN_FILE_PATH, word_to_index_dict, known_typo_dict)
+        test_text_1_list, test_text_2_list = load_file(TEST_FILE_PATH, word_to_index_dict, known_typo_dict)
 
         print("Initiating tokenizer ...")
         tokenizer = Tokenizer()
@@ -223,22 +206,16 @@ def load_dataset():
         assert np.sum(np.isclose(np.sum(embedding_matrix, axis=1), 0)) == 1
 
         print("Converting to numpy array ...")
-        train_frequency_1_array = np.array(train_frequency_1_list)
-        train_frequency_2_array = np.array(train_frequency_2_list)
         train_label_array = np.array(train_label_list, dtype=np.bool)
-        test_frequency_1_array = np.array(test_frequency_1_list)
-        test_frequency_2_array = np.array(test_frequency_2_list)
 
         print("Saving dataset to disk ...")
         np.savez_compressed(DATASET_FILE_PATH,
-                            train_data_1_array=train_data_1_array, train_frequency_1_array=train_frequency_1_array,
-                            train_data_2_array=train_data_2_array, train_frequency_2_array=train_frequency_2_array,
-                            test_data_1_array=test_data_1_array, test_frequency_1_array=test_frequency_1_array,
-                            test_data_2_array=test_data_2_array, test_frequency_2_array=test_frequency_2_array,
+                            train_data_1_array=train_data_1_array, train_data_2_array=train_data_2_array,
+                            test_data_1_array=test_data_1_array, test_data_2_array=test_data_2_array,
                             train_label_array=train_label_array, embedding_matrix=embedding_matrix)
 
-        return train_data_1_array, train_frequency_1_array, train_data_2_array, train_frequency_2_array, \
-            test_data_1_array, test_frequency_1_array, test_data_2_array, test_frequency_2_array, train_label_array, embedding_matrix
+        return train_data_1_array, train_data_2_array, test_data_1_array, test_data_2_array, \
+            train_label_array, embedding_matrix
 
 def init_model(embedding_matrix, learning_rate=0.002):
     def get_sentence_feature_extractor(embedding_matrix):
@@ -267,15 +244,11 @@ def init_model(embedding_matrix, learning_rate=0.002):
     # Initiate the input tensors
     input_data_1_tensor = Input(shape=(None,), dtype="int32")
     input_data_2_tensor = Input(shape=(None,), dtype="int32")
-    input_frequency_1_tensor = Input(shape=(1,))
-    input_frequency_2_tensor = Input(shape=(1,))
 
     # Define the sentence feature extractor
     sentence_feature_extractor = get_sentence_feature_extractor(embedding_matrix)
     input_1_feature_tensor = sentence_feature_extractor(input_data_1_tensor)
-    input_1_feature_tensor = merge([input_1_feature_tensor, input_frequency_1_tensor], mode="concat")
     input_2_feature_tensor = sentence_feature_extractor(input_data_2_tensor)
-    input_2_feature_tensor = merge([input_2_feature_tensor, input_frequency_2_tensor], mode="concat")
     merged_feature_1_tensor = merge([input_1_feature_tensor, input_2_feature_tensor], mode="concat")
     merged_feature_2_tensor = merge([input_2_feature_tensor, input_1_feature_tensor], mode="concat")
 
@@ -287,7 +260,7 @@ def init_model(embedding_matrix, learning_rate=0.002):
     output_tensor = Lambda(lambda x: K.mean(x, axis=1, keepdims=True), output_shape=(1,))(output_tensor)
 
     # Define the overall model
-    model = Model([input_data_1_tensor, input_data_2_tensor, input_frequency_1_tensor, input_frequency_2_tensor], output_tensor)
+    model = Model([input_data_1_tensor, input_data_2_tensor], output_tensor)
     model.compile(optimizer=Nadam(lr=learning_rate), loss="binary_crossentropy", metrics=["accuracy"])
     model.summary()
 
@@ -356,8 +329,7 @@ def run():
     os.makedirs(SUBMISSION_FOLDER_PATH, exist_ok=True)
 
     print("Loading dataset ...")
-    train_data_1_array, train_frequency_1_array, train_data_2_array, train_frequency_2_array, \
-        test_data_1_array, test_frequency_1_array, test_data_2_array, test_frequency_2_array, train_label_array, embedding_matrix = load_dataset()
+    train_data_1_array, train_data_2_array, test_data_1_array, test_data_2_array, train_label_array, embedding_matrix = load_dataset()
 
     print("Initializing model ...")
     model = init_model(embedding_matrix)
@@ -372,8 +344,8 @@ def run():
         earlystopping_callback = EarlyStopping(monitor="val_loss", patience=PATIENCE)
         modelcheckpoint_callback = ModelCheckpoint(OPTIMAL_WEIGHTS_FILE_RULE, monitor="val_loss", save_best_only=True, save_weights_only=True)
         inspectlossaccuracy_callback = InspectLossAccuracy()
-        model.fit([train_data_1_array[train_index_array], train_data_2_array[train_index_array], train_frequency_1_array[train_index_array], train_frequency_2_array[train_index_array]], train_label_array[train_index_array], batch_size=BATCH_SIZE,
-                validation_data=([train_data_1_array[valid_index_array], train_data_2_array[valid_index_array], train_frequency_1_array[valid_index_array], train_frequency_2_array[valid_index_array]], train_label_array[valid_index_array], valid_sample_weights),
+        model.fit([train_data_1_array[train_index_array], train_data_2_array[train_index_array]], train_label_array[train_index_array], batch_size=BATCH_SIZE,
+                validation_data=([train_data_1_array[valid_index_array], train_data_2_array[valid_index_array]], train_label_array[valid_index_array], valid_sample_weights),
                 callbacks=[earlystopping_callback, modelcheckpoint_callback, inspectlossaccuracy_callback],
                 class_weight=CLASS_WEIGHT, nb_epoch=MAXIMUM_EPOCH_NUM, verbose=2)
     else:
@@ -382,7 +354,7 @@ def run():
         print("Performing the testing procedure ...")
         submission_file_path = os.path.join(SUBMISSION_FOLDER_PATH, "Aurora.csv")
         if not os.path.isfile(submission_file_path):
-            prediction_array = model.predict([test_data_1_array, test_data_2_array, test_frequency_1_array, test_frequency_2_array], batch_size=BATCH_SIZE, verbose=2)
+            prediction_array = model.predict([test_data_1_array, test_data_2_array], batch_size=BATCH_SIZE, verbose=2)
             submission_file_content = pd.DataFrame({"test_id":np.arange(len(prediction_array)), "is_duplicate":prediction_array.flat})
             submission_file_content.to_csv(submission_file_path, index=False)
 
