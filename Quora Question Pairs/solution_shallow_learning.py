d54e7d4a93aca1dbefd03e51e7a7f49acbecb605
Xingyang Ni <nixingyang@users.noreply.github.com>
2017-05-26 19:32:46 +0300

    Add more features.

--- Quora Question Pairs/solution_shallow_learning.py
+++ Quora Question Pairs/solution_shallow_learning.py
@@ -263,24 +263,30 @@ def get_magic_feature(file_content):
     return file_content
 
 def load_extra_features():
-    print("Loading feature files ...")
-    train_file_content = pd.read_csv(os.path.join(EXTRA_FEATURES_FOLDER_PATH, "train_features.csv"), encoding="utf-8")
-    test_file_content = pd.read_csv(os.path.join(EXTRA_FEATURES_FOLDER_PATH, "test_features.csv"), encoding="utf-8")
+    def _load_extra_features(train_file_path, test_file_path):
+        print("Loading feature files ...")
+        train_file_content = pd.read_csv(train_file_path, encoding="utf-8")
+        test_file_content = pd.read_csv(test_file_path, encoding="utf-8")
 
-    print("Separating feature columns ...")
-    column_name_list = list(train_file_content)
-    question1_feature_column_name_list = sorted([column_name for column_name in column_name_list if "q1" in column_name])
-    question2_feature_column_name_list = sorted([column_name for column_name in column_name_list if "q2" in column_name])
-    common_feature_column_name_list = sorted(set(column_name_list) - set(question1_feature_column_name_list + question2_feature_column_name_list))
-    train_question1_feature_array = train_file_content[question1_feature_column_name_list].as_matrix().astype(np.float32)
-    train_question2_feature_array = train_file_content[question2_feature_column_name_list].as_matrix().astype(np.float32)
-    train_common_feature_array = train_file_content[common_feature_column_name_list].as_matrix().astype(np.float32)
-    test_question1_feature_array = test_file_content[question1_feature_column_name_list].as_matrix().astype(np.float32)
-    test_question2_feature_array = test_file_content[question2_feature_column_name_list].as_matrix().astype(np.float32)
-    test_common_feature_array = test_file_content[common_feature_column_name_list].as_matrix().astype(np.float32)
+        print("Separating feature columns ...")
+        column_name_list = list(train_file_content)
+        question1_feature_column_name_list = sorted([column_name for column_name in column_name_list if "q1" in column_name])
+        question2_feature_column_name_list = sorted([column_name for column_name in column_name_list if "q2" in column_name])
+        common_feature_column_name_list = sorted(set(column_name_list) - set(question1_feature_column_name_list + question2_feature_column_name_list))
+        train_question1_feature_array = train_file_content[question1_feature_column_name_list].as_matrix().astype(np.float32)
+        train_question2_feature_array = train_file_content[question2_feature_column_name_list].as_matrix().astype(np.float32)
+        train_common_feature_array = train_file_content[common_feature_column_name_list].as_matrix().astype(np.float32)
+        test_question1_feature_array = test_file_content[question1_feature_column_name_list].as_matrix().astype(np.float32)
+        test_question2_feature_array = test_file_content[question2_feature_column_name_list].as_matrix().astype(np.float32)
+        test_common_feature_array = test_file_content[common_feature_column_name_list].as_matrix().astype(np.float32)
 
-    return train_question1_feature_array, train_question2_feature_array, train_common_feature_array, \
-        test_question1_feature_array, test_question2_feature_array, test_common_feature_array
+        return train_question1_feature_array, train_question2_feature_array, train_common_feature_array, \
+            test_question1_feature_array, test_question2_feature_array, test_common_feature_array
+
+    train_file_path_list = sorted(glob.glob(os.path.join(EXTRA_FEATURES_FOLDER_PATH, "*_train_features.csv")))
+    test_file_path_list = sorted(glob.glob(os.path.join(EXTRA_FEATURES_FOLDER_PATH, "*_test_features.csv")))
+    for train_file_path, test_file_path in zip(train_file_path_list, test_file_path_list):
+        yield _load_extra_features(train_file_path, test_file_path)
 
 def load_dataset():
     if os.path.isfile(DATASET_FILE_PATH):
@@ -328,17 +334,15 @@ def load_dataset():
                             test_question1_feature_array=test_question1_feature_array, test_question2_feature_array=test_question2_feature_array,
                             test_common_feature_array=test_common_feature_array)
 
-    print("Loading extra features ...")
-    extra_train_question1_feature_array, extra_train_question2_feature_array, extra_train_common_feature_array, \
-            extra_test_question1_feature_array, extra_test_question2_feature_array, extra_test_common_feature_array = load_extra_features()
-
-    print("Merging the features ...")
-    train_question1_feature_array = np.hstack((train_question1_feature_array, extra_train_question1_feature_array))
-    train_question2_feature_array = np.hstack((train_question2_feature_array, extra_train_question2_feature_array))
-    train_common_feature_array = np.hstack((train_common_feature_array, extra_train_common_feature_array))
-    test_question1_feature_array = np.hstack((test_question1_feature_array, extra_test_question1_feature_array))
-    test_question2_feature_array = np.hstack((test_question2_feature_array, extra_test_question2_feature_array))
-    test_common_feature_array = np.hstack((test_common_feature_array, extra_test_common_feature_array))
+    print("Loading and merging extra features ...")
+    for extra_train_question1_feature_array, extra_train_question2_feature_array, extra_train_common_feature_array, \
+        extra_test_question1_feature_array, extra_test_question2_feature_array, extra_test_common_feature_array in load_extra_features():
+        train_question1_feature_array = np.hstack((train_question1_feature_array, extra_train_question1_feature_array))
+        train_question2_feature_array = np.hstack((train_question2_feature_array, extra_train_question2_feature_array))
+        train_common_feature_array = np.hstack((train_common_feature_array, extra_train_common_feature_array))
+        test_question1_feature_array = np.hstack((test_question1_feature_array, extra_test_question1_feature_array))
+        test_question2_feature_array = np.hstack((test_question2_feature_array, extra_test_question2_feature_array))
+        test_common_feature_array = np.hstack((test_common_feature_array, extra_test_common_feature_array))
 
     return train_question1_feature_array, train_question2_feature_array, train_common_feature_array, train_label_array, \
         test_question1_feature_array, test_question2_feature_array, test_common_feature_array
