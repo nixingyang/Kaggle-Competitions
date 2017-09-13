7ab51921ecfb2cd5da344380222adeb55dfeb92e
Xingyang Ni <nixingyang@users.noreply.github.com>
2017-09-13 21:50:42 +0300

    Fix performance issue.

--- Text Normalization Challenge/solution.py
+++ Text Normalization Challenge/solution.py
@@ -1,5 +1,6 @@
 import os
 import time
+import operator
 import pandas as pd
 
 # Dataset
@@ -11,29 +12,40 @@ TEST_FILE_PATH = os.path.join(VANILLA_DATASET_FOLDER_PATH, "en_test.csv")
 
 # Output
 SUBMISSION_FOLDER_PATH = os.path.join(PROJECT_FOLDER_PATH, "submission")
-SUBMISSION_FILE_PATH = os.path.join(SUBMISSION_FOLDER_PATH, "{}.csv".format(time.strftime("%c")))
 
 def run():
     print("Creating folders ...")
     os.makedirs(SUBMISSION_FOLDER_PATH, exist_ok=True)
 
     print("Loading {} ...".format(TRAIN_FILE_PATH))
-    before_to_after_dict = {}
-    before_to_count_dict = {}
-    file_content = pd.read_csv(TRAIN_FILE_PATH, encoding="utf-8")[["before", "after"]]
-    grouped_file_content = file_content.groupby(["before", "after"])
-    count_array = grouped_file_content.apply(lambda group: len(group)).as_matrix()  # pylint: disable=unnecessary-lambda
-    for (before, after), count in zip(grouped_file_content.groups.keys(), count_array):
-        if before in before_to_count_dict and before_to_count_dict[before] >= count:
-            continue
-        before_to_after_dict[before] = after
-        before_to_count_dict[before] = count
+    summary_dict = {}
+    file_content = pd.read_csv(TRAIN_FILE_PATH, usecols=["before", "after"], encoding="utf-8")
+    for before, after in file_content.itertuples(index=False):
+        if before not in summary_dict:
+            summary_dict[before] = {}
+        if after not in summary_dict[before]:
+            summary_dict[before][after] = 0
+        summary_dict[before][after] += 1
+
+    print("Generating lookup dict ...")
+    lookup_dict = {}
+    for before in summary_dict.keys():
+        after = max(summary_dict[before].items(), key=operator.itemgetter(1))[0]
+        if before != after:
+            lookup_dict[before] = after
 
     print("Loading {} ...".format(TEST_FILE_PATH))
-    file_content = pd.read_csv(TEST_FILE_PATH, encoding="utf-8")
-    file_content["id"] = file_content.apply(lambda row: "{}_{}".format(row["sentence_id"], row["token_id"]), axis=1)
-    file_content["after"] = file_content.apply(lambda row: before_to_after_dict.get(row["before"], row["before"]), axis=1)
-    file_content[["id", "after"]].to_csv(SUBMISSION_FILE_PATH, index=False, encoding="utf-8")
+    entry_list = []
+    file_content = pd.read_csv(TEST_FILE_PATH, usecols=["sentence_id", "token_id", "before"], encoding="utf-8")
+    for sentence_id, token_id, before in file_content.itertuples(index=False):
+        merged_id = "{}_{}".format(sentence_id, token_id)
+        after = lookup_dict.get(before, before)
+        entry_list.append((merged_id, after))
+
+    print("Writing submission ...")
+    submission_file_content = pd.DataFrame(entry_list, columns=["id", "after"])
+    submission_file_path = os.path.join(SUBMISSION_FOLDER_PATH, "{}.csv".format(time.strftime("%c")))
+    submission_file_content.to_csv(submission_file_path, index=False, encoding="utf-8")
 
     print("All done!")
 
