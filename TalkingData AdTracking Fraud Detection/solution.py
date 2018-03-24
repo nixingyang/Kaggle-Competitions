1df289f0be3847df05425da3bf19cbb189cbcc78
Xingyang Ni <nixingyang@users.noreply.github.com>
2018-03-24 18:51:34 +0200

    Add some hand-crafted features.

--- TalkingData AdTracking Fraud Detection/solution.py
+++ TalkingData AdTracking Fraud Detection/solution.py
@@ -45,6 +45,34 @@ def load_data(nrows=SAMPLE_NUM):
     merged_df["hour"] = merged_df["click_time"].dt.hour.astype("uint8")
     merged_df.drop("click_time", axis=1, inplace=True)
 
+    print("Grouping by ip-day-hour ...")
+    temp_df = merged_df[["ip", "day", "hour", "channel"]].groupby(by=["ip", "day", "hour"])[["channel"]].count().reset_index().rename(index=str, columns={"channel": "ip_tcount"})
+    merged_df = merged_df.merge(temp_df, on=["ip", "day", "hour"], how="left")
+
+    print("Grouping by ip-app ...")
+    temp_df = merged_df[["ip", "app", "channel"]].groupby(by=["ip", "app"])[["channel"]].count().reset_index().rename(index=str, columns={"channel": "ip_app_count"})
+    merged_df = merged_df.merge(temp_df, on=["ip", "app"], how="left")
+
+    print("Grouping by ip-app-os ...")
+    temp_df = merged_df[["ip", "app", "os", "channel"]].groupby(by=["ip", "app", "os"])[["channel"]].count().reset_index().rename(index=str, columns={"channel": "ip_app_os_count"})
+    merged_df = merged_df.merge(temp_df, on=["ip", "app", "os"], how="left")
+
+    print("Grouping by ip_day_chl_var_hour ...")
+    temp_df = merged_df[["ip", "day", "hour", "channel"]].groupby(by=["ip", "day", "channel"])[["hour"]].var().reset_index().rename(index=str, columns={"hour": "ip_tchan_count"})
+    merged_df = merged_df.merge(temp_df, on=["ip", "day", "channel"], how="left")
+
+    print("Grouping by ip_app_os_var_hour ...")
+    temp_df = merged_df[["ip", "app", "os", "hour"]].groupby(by=["ip", "app", "os"])[["hour"]].var().reset_index().rename(index=str, columns={"hour": "ip_app_os_var"})
+    merged_df = merged_df.merge(temp_df, on=["ip", "app", "os"], how="left")
+
+    print("Grouping by ip_app_channel_var_day ...")
+    temp_df = merged_df[["ip", "app", "channel", "day"]].groupby(by=["ip", "app", "channel"])[["day"]].var().reset_index().rename(index=str, columns={"day": "ip_app_channel_var_day"})
+    merged_df = merged_df.merge(temp_df, on=["ip", "app", "channel"], how="left")
+
+    print("Grouping by ip_app_chl_mean_hour ...")
+    temp_df = merged_df[["ip", "app", "channel", "hour"]].groupby(by=["ip", "app", "channel"])[["hour"]].mean().reset_index().rename(index=str, columns={"hour": "ip_app_channel_mean_hour"})
+    merged_df = merged_df.merge(temp_df, on=["ip", "app", "channel"], how="left")
+
     print("Splitting data ...")
     train_indexes, valid_indexes = train_test_split(np.arange(train_num), test_size=0.1, random_state=0)
     train_df, valid_df = merged_df.iloc[train_indexes], merged_df.iloc[valid_indexes]
